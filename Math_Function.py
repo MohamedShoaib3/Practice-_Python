767af520194551ca21db756982b7899207f7816b
MohamedShoaib3 <mohamedshoaib253@gmail.com>
2024-10-18 16:28:38 +0300

    Create Math_Function.py

---
+++ Math_Function.py
@@ -0,0 +1,11 @@
+def min (num1 , num2 ):
+     if num1 > num2:
+         print (num2)
+     else :
+         print (num1)
+       
+def max (num1 , num2 ):
+     if num1 > num2:
+         print (num1)
+     else :
+         print (num2)
