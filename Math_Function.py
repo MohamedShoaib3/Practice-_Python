a4113f1f6339cc74ca4abe9ab2291976916b9521
MohamedShoaib3 <mohamedshoaib253@gmail.com>
2024-10-18 16:45:39 +0300

    Update Math_Function.py

--- Math_Function.py
+++ Math_Function.py
@@ -9,3 +9,16 @@ def max (num1 , num2 ):
          print (num1)
      else :
          print (num2)
+
+ def absolute (num):
+    if num < 0 :
+        return -num
+    return num      
+
+def msx (num1 , num2 ):
+    num1 = absolute (num1)
+    num2 = absolute (num2)
+     if num1 > num2:
+         return (num1)
+     else :
+          return (num2)
